26fe0350b887388aa344d76c015e04a6ef434dbe
Pia Kochar <piakochar11@gmail.com>
2015-12-07 17:55:02 -0500

    added pictures locally and implemented filtering by tag

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-var mongo = require('/db/mongo');
-
 $(document).ready(function () {
-  
+  var $pictureGrid = $('#picture-grid');
+  var grid = new PictureGrid($pictureGrid);
+  grid.populateGrid();
+  $('#filter').click(function () {
+  	grid.filterTag($('#tag').val());
+  });
+  $('#unfilter').click(function () {
+  	grid.removeFilters();
+  })
+  var $addPic = $('#add-pic');
+  $addPic.on('click', function () {
+  	console.log('Picture added!');
+  });
 });
